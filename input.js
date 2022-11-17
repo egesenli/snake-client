f898d5a0b62de04a5c564fc090a511b42146680f
egesenli <egesenli@gmail.com>
2022-11-17 03:33:08 -0500

    WASD movement has one and refactored input.

--- input.js
+++ input.js
@@ -1,5 +1,8 @@
-const setupInput = function() {
-  const stdin = process.stdin;
+let connection;
+
+const setupInput = function(conn) {
+  connection = conn;
+const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
   stdin.resume();
@@ -14,6 +17,18 @@ const handleUserInput = (key) => {
   if (key === '\u0003') {
     stdout.write("Exited snek game. Bye bye.\n");
     process.exit();
+  }
+  if (key === 'w' || key === 'W') {
+    connection.write("Move: up");
+  }
+  if (key === 'a' || key === 'A' ) {
+    connection.write("Move: left");
+  }
+  if (key === 's' || key === 'S') {
+    connection.write("Move: down");
+  }
+  if (key === 'd' || key === 'D') {
+    connection.write("Move: right");
   }
 };
 
